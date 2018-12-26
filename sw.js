442c1c9e63b39057089c0e648c8b620971ddc992
julio gonzalez valdes <jogv66@gmail.com>
2018-12-26 12:47:24 -0300

    modificacion del ruta del sw

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const INMUTABLE_CACHE = 'inmutable-v1';
 
 // todo lo que nunca se modificará
 const APP_SHELL = [
-    '/',
+    // '/',
     'index.html',
     'css/style.css',
     'img/favicon.ico',
